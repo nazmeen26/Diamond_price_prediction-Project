7367e0c06fadf09d9833b0089e1c823cc1df643f
nazmeen26 <khannazmeen7860@gmail.com>
2024-08-17 04:00:12 +0530

    data ingestion

---
+++ src/pipelines/training_pipeline.py
@@ -0,0 +1,16 @@
+import os
+import sys
+sys.path.append(os.path.join('D:/skills/DiamondPrice'))
+from src.logger import logging
+from src.exception import CustomException
+import pandas as pd
+
+from src.components.data_ingestion import DataIngestion
+
+
+
+
+if __name__=='__main__':
+    obj=DataIngestion()
+    train_data_path,test_data_path=obj.initiate_data_ingestion()
+    print(train_data_path,test_data_path)
